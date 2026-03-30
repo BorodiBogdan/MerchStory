36e583105d1aa893332f59c0d5ad086201ba5af4
BorodiBogdan <bogdanborodiv@gmail.com>
2026-03-30 22:40:30 +0300

    add migrations

---
+++ backend/MerchStoryAPI/Migrations/20260330194003_AddGenerationTypeToGeneratedImage.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MerchStoryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenerationTypeToGeneratedImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GenerationType",
+                table: "GeneratedImages",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GenerationType",
+                table: "GeneratedImages");
+        }
+    }
+}
